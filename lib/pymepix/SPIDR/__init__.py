db10b51f0b16b05172d5f722af1f0ef8989459cb
alrefaie <alrefaie@desy.de>
2018-09-05 19:26:39 +0200

    accidental enum definition in __init__ removed

---
+++ lib/pymepix/SPIDR/__init__.py
@@ -0,0 +1,6 @@
+import enum
+
+class SPIDRReadoutSpeed(Enum):
+    HighSpeed = 0x89ABCDEF #High speed magic number
+    LowSpeed = 0x12345678 #Low speed magic number
+    Default = 0 #Use default readout speed
