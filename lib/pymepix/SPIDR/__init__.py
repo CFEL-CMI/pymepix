7d5b5d26e98c480ac801e644a3fffa4ed2cd0602
alrefaie <alrefaie@desy.de>
2018-09-06 12:12:39 +0200

    cleared spider init file

--- lib/pymepix/SPIDR/__init__.py
+++
@@ -1,6 +0,0 @@
-import enum
-
-class SPIDRReadoutSpeed(Enum):
-    HighSpeed = 0x89ABCDEF #High speed magic number
-    LowSpeed = 0x12345678 #Low speed magic number
-    Default = 0 #Use default readout speed
