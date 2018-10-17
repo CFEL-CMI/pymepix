9fa32d9f8d6ac895fd5a11a05988b32d21a2eac8
alrefaie <alrefaie@desy.de>
2018-10-17 16:41:17 +0200

    ensure timer is an int

--- lib/pymepix/fakepacket.py
+++ lib/pymepix/fakepacket.py
@@ -35,7 +35,7 @@ class FakePacket(object):
 
 
         #Move it back by one second
-        self._current_time = self.compute_new_time(lsb,msb)- (1E9//25)
+        self._current_time = int(self.compute_new_time(lsb,msb)- (1E9//25))
         print(self._current_time*25E-9)
 
         self._file.seek(0)
@@ -45,37 +45,37 @@ class FakePacket(object):
     def run(self):
 
 
-            buffer = self._file.read(8192*1000) # buffer size is 1024 bytes
-            if buffer:
-                packet = np.frombuffer(buffer,dtype=np.uint64)
-                #self._file_queue.put(('WRITE',raw_packet))
-                header = ((packet & 0xF000000000000000) >> 60) & 0xF
-                subheader = ((packet & 0x0F00000000000000) >> 56) & 0xF
+        buffer = self._file.read(8192*1000) # buffer size is 1024 bytes
+        
+        if buffer:
+            packet = np.frombuffer(buffer,dtype=np.uint64)
+            #self._file_queue.put(('WRITE',raw_packet))
+            header = ((packet & 0xF000000000000000) >> 60) & 0xF
+            subheader = ((packet & 0x0F00000000000000) >> 56) & 0xF
 
-                trigger_header = (header==0x4)|(header==0x6)
-                lsb_time_filter =np.where(trigger_header & (subheader == 0x4))[0]
-                msb_time_filter = np.where(trigger_header & (subheader == 0x5))[0]
-                #print(msb_time_filter)
-                start_index = 0
+            trigger_header = (header==0x4)|(header==0x6)
+            lsb_time_filter =np.where(trigger_header & (subheader == 0x4))[0]
+            msb_time_filter = np.where(trigger_header & (subheader == 0x5))[0]
+            #print(msb_time_filter)
+            start_index = 0
 
-                if msb_time_filter.size > 0:
+            if lsb_time_filter.size > 0 and msb_time_filter.size > 0:
+                
+                for x in range(min(lsb_time_filter.size,msb_time_filter.size)):
                     
-                    for x in range(msb_time_filter.size):
-                        
-                        end_index = msb_time_filter[x]
-                        _packet = packet[start_index:end_index]
-                        print(start_index,end_index)
-                        self.upload_packet(_packet)
+                    end_index = msb_time_filter[x]
+                    _packet = packet[start_index:end_index]
+                    self.upload_packet(_packet)
 
-                        lsb = packet[lsb_time_filter[x]]
-                        msb = packet[msb_time_filter[x]]
-                        self._current_time = self.compute_new_time(lsb,msb)
-                        start_index = end_index
-                        #print(self._current_time*25E-9)
-                    self.upload_packet(packet[start_index:])
+                    lsb = packet[lsb_time_filter[x]]
+                    msb = packet[msb_time_filter[x]]
+                    self._current_time = self.compute_new_time(lsb,msb)
+                    start_index = end_index
+                    #print(self._current_time*25E-9)
+                self.upload_packet(packet[start_index:])
 
-                else:
-                    self.upload_packet(packet)               
+            else:
+                self.upload_packet(packet)               
 
     
     def upload_packet(self,packet):
@@ -99,19 +99,59 @@ class FakePacket(object):
         tmplongtime = (longtime_msb | longtime_lsb)
         return tmplongtime
 
+def goodPart(queue,plt2):
+    import time
+    hy = None
+    hx = None
+
+    start = time.time()
+    print('HISTO')
+    for item in iter(queue.get, None):
+        for trigger_counter,start,x,y,toa,tot in item:
+            tof = toa-start
+            _y,_x = np.histogram(tof,np.linspace(52E-6,58E-6,1000))
+            if hy is None:
+                hy = _y
+                hx = _x
+            else:
+                hy+=_y
+            
+        end = time.time()
+        if (end-start) > 2:
+            print('Event ')
+            plt2.setData(x=hx, y=hy, stepMode=True, fillLevel=0, brush=(0,0,255,150))
+            start = end
+
+    print('DONE')
+
 def main():
     from multiprocessing import Queue
-    import matplotlib.pyplot
+    import pyqtgraph as pg
+    from pyqtgraph.Qt import QtCore, QtGui
+    import threading
+    win = pg.GraphicsWindow()
+    win.resize(800,350)
+    win.setWindowTitle('pyqtgraph example: Histogram')
+    plt1 = win.addPlot()    
+    plt1.setLabel('bottom',text='Time of Flight',units='s')
+    plt1.setLabel('left',text='Hits')
+    tof_data = pg.PlotDataItem()
+    plt1.addItem(tof_data)
     pixq = Queue()
     evnt = Queue()
     pp=PacketProcessor(pixq,evnt)
     pp.start()
-    fp = FakePacket('/Users/alrefaie/Documents/repos/libtimepix/lib/onlineviewer/coin4-1.dat',pixq,)
+    fp = FakePacket('/Users/alrefaie/Documents/repos/libtimepix/lib/onlineviewer/coin4-1.dat',pixq)
     fp.run()
     pixq.put(None)
+    t = threading.Thread(target=goodPart,args=(evnt,tof_data,))
+    t.start()
+
+    
+    
+    QtGui.QApplication.instance().exec_()
+
     pp.join()
-
-
 
 if __name__=='__main__':
     main()
