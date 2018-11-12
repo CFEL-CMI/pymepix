528857b4ad4cb6b8dea0ae8eda8faf5d7922f4d3
alrefaie <alrefaie@desy.de>
2018-11-12 14:56:43 +0100

    Setup timepix config base class

---
+++ lib/pymepix/config/timepixconfig.py
@@ -0,0 +1,28 @@
+
+
+
+
+class TimepixConfig(object):
+
+    def __init__(self):
+        pass
+        
+
+        
+    
+    def dacCodes(self):
+        """ Returns an iterator with format daccode,value"""
+        pass
+    
+    def maskPixels(self):
+        """Returns mask pixels"""
+        pass
+    
+    def testPixels(self):
+        """Returns test pixels"""
+        pass
+    
+    def thresholdPixels(self):
+        """Returns thrshold pixels"""
+        pass
+
