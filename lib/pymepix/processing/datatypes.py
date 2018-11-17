f2c192ad3aca93f76b4cce00499246383e4d66a3
alrefaie <alrefaie@desy.de>
2018-11-17 09:40:28 +0100

    Defined some data that is passed though a pipeline

--- lib/pymepix/processing/datatypes.py
+++ lib/pymepix/processing/datatypes.py
@@ -4,7 +4,17 @@ from enum import IntEnum
 
 class MessageType(IntEnum):
     """Defines the type of message that is being passed into a multiprocessing queue"""
-    pass
+    RawData = 0
+    """Raw UDP packets"""
+    PixelData = 1
+    """Decoded Pixel/Trigger Data"""
+    EventData = 2
+    """Event Data"""
+    CentroidData = 3
+    """Centroided Data"""
+
+
+
 
 
     
