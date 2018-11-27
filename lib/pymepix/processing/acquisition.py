94bd03e2fb81032f1388cd0bd0c63cf1a2bf02bc
alrefaie <alrefaie@desy.de>
2018-11-27 18:22:49 +0100

    Added event window selection in pipeline

--- lib/pymepix/processing/acquisition.py
+++ lib/pymepix/processing/acquisition.py
@@ -12,10 +12,21 @@ class PixelPipeline(AcquisitionPipeline):
         AcquisitionPipeline.__init__(self,'Pixel',data_queue)
 
         self._use_events = False
+        self._event_window = (0,10000)
 
         self.addStage(0,UdpSampler,address,longtime)
-        
-        self.addStage(2,PacketProcessor,handle_events=False)
+
+        self.addStage(2,PacketProcessor,handle_events=False,event_window=self._event_window)
 
 
-
+    @property
+    def eventWindow(self):
+        return self._event_window
+    
+    @eventWindow.setter
+    def eventWindow(self,value):
+        self._event_window = value
+        if self.isRunning:
+            min_win,max_win = self._event_window
+            self.getStage(2).processes[-1].minWindow = min_win
+            self.getStage(2).processes[-1].maxWindow = max_win
