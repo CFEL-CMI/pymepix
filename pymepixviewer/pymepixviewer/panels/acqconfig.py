b1d858ba708298ed3fb0ba4cdbc795c52325a4a0
alrefaie <alrefaie@desy.de>
2018-12-03 16:55:36 +0100

    bias voltage and mode change handled by acqconfig

--- pymepixviewer/pymepixviewer/panels/acqconfig.py
+++ pymepixviewer/pymepixviewer/panels/acqconfig.py
@@ -1,6 +1,7 @@
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 from .ui.acqconfigui import Ui_Form
+from ..core.datatypes import ViewerMode
 import logging
 
 logger = logging.getLogger(__name__)
@@ -12,6 +13,9 @@ class AcquisitionConfig(QtGui.QWidget,Ui_Form):
     updateRateChange = QtCore.pyqtSignal(float)
     eventCountChange = QtCore.pyqtSignal(int)   
     frameTimeChange = QtCore.pyqtSignal(float)
+    biasVoltageChange = QtCore.pyqtSignal(int)
+
+    modeChange = QtCore.pyqtSignal(object)
 
     def __init__(self,parent=None):
         super(AcquisitionConfig, self).__init__(parent)
@@ -19,9 +23,11 @@ class AcquisitionConfig(QtGui.QWidget,Ui_Form):
         # Set up the user interface from Designer.
         self.setupUi(self)  
 
+        self._current_mode = ViewerMode.TOA
+
         self.setupLines()
         self.connectSignals()
-
+        self.handleMode()
     def setupLines(self):
         self.event_count.setValidator(QtGui.QIntValidator(self))
         self.acq_time.setValidator(QtGui.QDoubleValidator(self))
@@ -33,7 +39,8 @@ class AcquisitionConfig(QtGui.QWidget,Ui_Form):
         self.event_count.returnPressed.connect(self.eventCountChanged)
         self.frame_time.returnPressed.connect(self.frameTimeChanged)
         self.reset_plots.clicked.connect(self.resetPlots.emit)
-
+        self.bias_voltage.valueChanged[int].connect(self.biasVoltageChange.emit)
+        self.mode_box.currentIndexChanged[int].connect(self.modeChanged)
     def openPath(self):
         directory = QtGui.QFileDialog.getExistingDirectory(self, "Open Directory",
                                              "/home",
@@ -41,6 +48,30 @@ class AcquisitionConfig(QtGui.QWidget,Ui_Form):
                                              | QtGui.QFileDialog.DontResolveSymlinks)
 
         self.path_name.setText(directory)
+
+    def modeChanged(self,value):
+        
+        if value == 0:
+            self.modeChange.emit(ViewerMode.TOA)
+            self._current_mode = ViewerMode.TOA
+        elif value == 1:
+            self.modeChange.emit(ViewerMode.TOF)
+            self._current_mode = ViewerMode.TOF
+        elif value == 2:
+            self.modeChange.emit(ViewerMode.Centroid)
+            self._current_mode = ViewerMode.Centroid
+        
+        self.handleMode()
+    def handleMode(self):
+
+
+        if self._current_mode is ViewerMode.TOA:
+            self.eventwidget.hide()
+            self.framelayout.show()
+        elif self._current_mode in (ViewerMode.TOF,ViewerMode.Centroid,):
+            self.eventwidget.show()
+            self.framelayout.hide()
+        
 
     def displayRateChange(self,value):
         seconds = 1.0/value
@@ -60,4 +91,4 @@ class AcquisitionConfig(QtGui.QWidget,Ui_Form):
         
         logger.info('Frame time changed to {} s'.format(frame_time))
 
-        self.updateRateChange.emit(frame_time)        
+        self.frameTimeChange.emit(frame_time)        
