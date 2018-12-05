d156ed4acabf94a0d1af6ea4d7f084cc56e494be
alrefaie <alrefaie@desy.de>
2018-12-05 12:15:06 +0000

    used open functions from pymepix

--- pymepixviewer/pymepixviewer/core/filesaver.py
+++ pymepixviewer/pymepixviewer/core/filesaver.py
@@ -1,5 +1,6 @@
 from pyqtgraph.Qt import QtCore, QtGui
-from pymepix.util.storage import open_output_file
+from pymepix.util.storage import open_output_file,store_raw,store_toa,store_tof,store_centroid
+from pymepix.processing import MessageType
 class FileSaver(QtCore.QThread):
 
     def __init__(self):
@@ -14,21 +15,37 @@ class FileSaver(QtCore.QThread):
         if self._raw_file is not None:
             self._raw_file.close()
         
-        self._raw_file = open_output_file(filename,'.raw',index=index)
+        self._raw_file = open_output_file(filename,'.raw',index=self._index)
     
     def openTof(self,filename):
         if self._tof_file is not None:
             self._tof_file.close()
         
-        self._tof_file = open_output_file(filename,'.tof',index=index)
+        self._tof_file = open_output_file(filename,'.tof',index=self._index)
     
     
     def openBlob(self,filename):
         if self._blob_file is not None:
             self._blob_file.close()
         
-        self._blob_file = open_output_file(filename,'.blob',index=index)
+        self._blob_file = open_output_file(filename,'.blob',index=self._index)
     
+    def setIndex(self,index):
+        self._index = index
+
+    def processData(self,data):
+        data_type,result = data
+
+        if data_type is MessageType.RawData:
+            if self._raw_file is not None:
+                store_raw(self._raw_file,data)
+
+        elif data_type is MessageType.EventData:
+            if self._tof_file is not None:
+                store_tof(self._tof_file,data)       
+        elif data_type is MessageType.CentroidData:
+            if self._blob_file is not None:
+                store_centroid(self._blob_file,data) 
 
     def close(self):
         if self._raw_file is not None:
