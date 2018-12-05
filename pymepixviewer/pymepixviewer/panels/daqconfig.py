3fb68a8227d886d11c2f64daaede3750d428d9e4
alrefaie <alrefaie@desy.de>
2018-12-05 14:54:53 +0000

    Implemented procedure to collect acquisition settings

--- pymepixviewer/pymepixviewer/panels/daqconfig.py
+++ pymepixviewer/pymepixviewer/panels/daqconfig.py
@@ -1,9 +1,14 @@
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 from .ui.daqconfigui import Ui_Form
+from ..core.filesaver import FileSaver
 import threading
 from threading import Thread
-import time
+import time,os
+import logging
+
+logger = logging.getLogger(__name__)
+
 class RepeatFunction(Thread):
     """Call a function after a specified number of seconds:
 
@@ -34,10 +39,8 @@ class RepeatFunction(Thread):
 
 class DaqConfigPanel(QtGui.QWidget,Ui_Form):
 
-    startAcquisition = QtCore.pyqtSignal(str,str,bool,bool,float,int)
-    stopAcquisition = QtCore.pyqtSignal()
 
-    # resetPlots = QtCore.pyqtSignal()
+    resetPlots = QtCore.pyqtSignal()
     # updateRateChange = QtCore.pyqtSignal(float)
     # eventCountChange = QtCore.pyqtSignal(int)
 
@@ -53,6 +56,23 @@ class DaqConfigPanel(QtGui.QWidget,Ui_Form):
     #             time.sleep(time_val)
     #     print('ENDING')
     #     self.endAcquisition()
+    def run_acquisition(self,path_name,prefix,raw_checked,blob_checked,exposure,startindex):
+        self._in_acq = True
+        self.startAcquisition.emit(path_name,prefix,raw_checked,blob_checked,exposure,startindex)
+        self.text_status.setText('Acquiring.....')        
+        print('STARTING')
+        start = time.time()
+        if self.acq_time.text() != "":
+            time_val = float(self.acq_time.text())
+            if time_val != -1:
+                start = time.time()
+                while time.time() - start < time_val and self._in_acq:
+                    time.sleep(0.5)
+
+        tot_time = time.time()-start
+        print('ENDING, time taken {}s or {} minutes'.format(tot_time,tot_time/60.0))
+        self.endAcquisition()
+
 
 
     def __init__(self,parent=None):
@@ -61,16 +81,13 @@ class DaqConfigPanel(QtGui.QWidget,Ui_Form):
         # Set up the user interface from Designer.
         self.setupUi(self)  
 
-        self.setupLines()
-        # self.connectSignals()
 
-        # self._acq_thread = None
+        self.connectSignals()
 
-        # self._repeating_thread = None
+        self._repeating_thread = None
 
 
-    def setupLines(self):
-        pass
+
 
 
     def connectSignals(self):
@@ -80,7 +97,7 @@ class DaqConfigPanel(QtGui.QWidget,Ui_Form):
 
         self.start_acq.clicked.connect(self.startAcqClicked)
         self.end_acq.clicked.connect(self.endAcqClicked)
-        self.reset_plots.clicked.connect(self.resetPlots.emit)
+        self.acqtab.reset_plots.clicked.connect(self.resetPlots.emit)
 
     def openPath(self):
         directory = QtGui.QFileDialog.getExistingDirectory(self, "Open Directory",
@@ -99,9 +116,29 @@ class DaqConfigPanel(QtGui.QWidget,Ui_Form):
         self.eventCountChange.emit(int(self.event_count.text()))
 
 
+    def _collectAcquisitionSettings(self):
+        acq = self.acqtab
+
+        filename = os.path.join(acq.path_name.text(),acq.file_prefix.text())
+        logger.info('Filename to store to: {}'.format(filename))
+        index = acq.startindex.value()
+        logger.info('Start index is {}'.format(index))
+        raw_checked = bool(acq.write_raw.isChecked())
+        pixels_checked = bool(acq.write_pixels.isChecked())
+        tof_checked = bool(acq.write_tof.isChecked())
+        blob_checked = bool(acq.write_blob.isChecked())
+
+        logger.info('File settings: raw:{} toa:{} tof:{} blob:{}'.format(raw_checked,pixels_checked,tof_checked,blob_checked))
+
+        acq_time = float(acq.acq_time.text())
+        logger.info('Acq time is {} s'.format(acq_time))
+
+        repeats = int(acq.repeat_value.value())
+        logger.info('Will repeat this {} times'.format(repeats))
+
     def startAcqClicked(self):
 
-        print(self.path_name.text(),self.file_prefix.text())
+        self._collectAcquisitionSettings()
     #     exposure = 10000.0
     #     if self.exposure_time.text() != "":
     #         exposure = float(self.exposure_time.text())*1e-6
@@ -132,12 +169,16 @@ class DaqConfigPanel(QtGui.QWidget,Ui_Form):
     def endAcquisition(self):
         self.stopAcquisition.emit()
         self.text_status.setText('Live')
-
+        self._in_acq = False
+        self._elapsed_time.restart()
+    
     def endAcqClicked(self):
         self.endAcquisition()
-    #     if self._repeating_thread is not None:
-    #         self._repeating_thread.cancel()
-    #         self._repeating_thread = None
+        if self._repeating_thread is not None:
+            self._repeating_thread.cancel()
+            self._repeating_thread = None
+
+        
 def main():
     import sys
     app = QtGui.QApplication([])
