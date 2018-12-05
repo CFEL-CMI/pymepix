e8f150305dce67168736dd80884b889d477d4b27
alrefaie <alrefaie@desy.de>
2018-12-05 14:12:10 +0000

    FileSave is QObject now and can save toa data

--- pymepixviewer/pymepixviewer/core/filesaver.py
+++ pymepixviewer/pymepixviewer/core/filesaver.py
@@ -1,14 +1,21 @@
 from pyqtgraph.Qt import QtCore, QtGui
 from pymepix.util.storage import open_output_file,store_raw,store_toa,store_tof,store_centroid
 from pymepix.processing import MessageType
-class FileSaver(QtCore.QThread):
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class FileSaver(QtCore.QObject):
 
     def __init__(self):
-        QtCore.QThread.__init__(self)
+        QtCore.QObject.__init__(self)
 
         self._raw_file = None
         self._blob_file = None
         self._tof_file = None
+        self._toa_file= None
         self._index = 0
 
     def openRaw(self,filename):
@@ -16,7 +23,14 @@ class FileSaver(QtCore.QThread):
             self._raw_file.close()
         
         self._raw_file = open_output_file(filename,'.raw',index=self._index)
-    
+
+    def openToa(self,filename):
+        if self._toa_file is not None:
+            self._toa_file.close()
+        
+        self._toa_file = open_output_file(filename,'.toa',index=self._index)
+
+
     def openTof(self,filename):
         if self._tof_file is not None:
             self._tof_file.close()
@@ -40,6 +54,10 @@ class FileSaver(QtCore.QThread):
             if self._raw_file is not None:
                 store_raw(self._raw_file,data)
 
+        elif data_type is MessageType.PixelData:
+            if self._toa_file is not None:
+                store_toa(self._toa_file,data)    
+
         elif data_type is MessageType.EventData:
             if self._tof_file is not None:
                 store_tof(self._tof_file,data)       
@@ -57,3 +75,6 @@ class FileSaver(QtCore.QThread):
         if self._tof_file is not None:
             self._tof_file.close()
             self._tof_file = None
+        if self._toa_file is not None:
+            self._toa_file.close()
+            self._toa_file = None
