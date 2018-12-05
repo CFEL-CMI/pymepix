2044bf95db36a6673f73d4e467d2587377a8483a
alrefaie <alrefaie@desy.de>
2018-12-05 14:54:38 +0000

    File saver now can open all files at once

--- pymepixviewer/pymepixviewer/core/filesaver.py
+++ pymepixviewer/pymepixviewer/core/filesaver.py
@@ -18,30 +18,39 @@ class FileSaver(QtCore.QObject):
         self._toa_file= None
         self._index = 0
 
+
+    def openFile(self,filename,index,raw,toa,tof,blob):
+        self._index = index
+        if raw: self.openRaw(filename)
+        if toa: self.openToa(filename)
+        if tof: self.openTof(filename)
+        if blob: self.openBlob(filename)
+
+
     def openRaw(self,filename):
         if self._raw_file is not None:
             self._raw_file.close()
-        
+        logger.info('Opening raw file :{}'.format(filename))
         self._raw_file = open_output_file(filename,'.raw',index=self._index)
 
     def openToa(self,filename):
         if self._toa_file is not None:
             self._toa_file.close()
-        
+        logger.info('Opening toa file :{}'.format(filename))
         self._toa_file = open_output_file(filename,'.toa',index=self._index)
 
 
     def openTof(self,filename):
         if self._tof_file is not None:
             self._tof_file.close()
-        
+        logger.info('Opening tof file :{}'.format(filename))
         self._tof_file = open_output_file(filename,'.tof',index=self._index)
     
     
     def openBlob(self,filename):
         if self._blob_file is not None:
             self._blob_file.close()
-        
+        logger.info('Opening blob file :{}'.format(filename))
         self._blob_file = open_output_file(filename,'.blob',index=self._index)
     
     def setIndex(self,index):
@@ -67,14 +76,18 @@ class FileSaver(QtCore.QObject):
 
     def close(self):
         if self._raw_file is not None:
+            logger.info('Closing raw file')
             self._raw_file.close()
             self._raw_file = None
         if self._blob_file is not None:
+            logger.info('Closing blob file')
             self._blob_file.close()
             self._blob_file = None
         if self._tof_file is not None:
+            logger.info('Closing tof file')
             self._tof_file.close()
             self._tof_file = None
         if self._toa_file is not None:
+            logger.info('Closing toa file')
             self._toa_file.close()
             self._toa_file = None
