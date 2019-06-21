d2abac191e488089d9cbf85dd9bd0591cced368e
Ahmed Al-Refaie <alrefaie@desy.de>
2019-06-21 13:51:22 +0100

    Included GPLv3 license and license header on all files

--- pymepix/pymepix/core/log.py
+++ pymepix/pymepix/core/log.py
@@ -1,20 +1,24 @@
-# -*- coding: utf-8 -*-
+##############################################################################
+##
+# This file is part of Pymepix
 #
-# This file is part of pymepix
+# https://arxiv.org/abs/1905.07999
 #
-# This program is free software: you can redistribute it and/or modify it under the terms of the GNU
-# General Public License as published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
 #
-# If you use this programm for scientific work, you should correctly reference it; see LICENSE file
-# for details.
+# Pymepix is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
+# Pymepix is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along with this program. If not,
-# see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pymepix.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 
 import logging
