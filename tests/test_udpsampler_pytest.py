ecba9220970a02d4f61bdf65dd8cc1527952d4ce
Hubertus Bromberger <hubertus.bromberger@cfel.de>
2020-07-31 12:45:08 +0200

    test rudimentary receiving functionality

--- tests/test_udpsampler_pytest.py
+++ tests/test_udpsampler_pytest.py
@@ -18,7 +18,93 @@
 # You should have received a copy of the GNU General Public License along with this program. If not,
 # see <https://www.gnu.org/licenses/>.
 
-import pytest
-import pymepix.processing.udpsampler as udpsampler
+'''
+module to test udpsampler functionality
+run: pytest test_udpsampler_pytest.py
+'''
+import socket
+import ctypes
+import time
+import numpy as np
+from multiprocessing.sharedctypes import Value
+from pymepix.processing.udpsampler import UdpSampler
+
+def test_receive():
+    '''
+    most basic test for receiving functionality done in UdpSampler.process
+    '''
+    address = ('127.0.0.1', 50000)
+    longtime = Value(ctypes.c_int, 1)
+    sampler = UdpSampler(address, longtime=longtime)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(bytes(8), address)
+    #time.sleep(0.1)
+
+    sampler.preRun()
+    time.sleep(0.4) # sleep for (flush_time > self._flush_timeout) to become true
+    data = sampler.process()
+    assert sampler._packets_collected == 1
+    assert data[0] == 0 # should return <MessageType.RawData: 0>
+    assert data[1][1] == 1 # provided longtime gets returned
+    assert len(data[1][0]) == 1
+    assert data[1][0].sum() == 0
+
+    # send some more data but due to too short time between last check (preRun), data should not be flushed
+    sampler.preRun()
+    sock.sendto(bytes(64), address)
+    data = sampler.process()
+    assert sampler._packets_collected == 2
+    assert data[0] == None
+    assert data[1] == None
+
+    # No new packets sent, thus socket should time-out
+    sampler.preRun()
+    time.sleep(0.4)
+    data = sampler.process()
+    assert sampler._packets_collected == 2
+    assert data[0] == None
+    assert data[1] == None
+
+    # send some more data
+    sampler.preRun()
+    time.sleep(0.4)
+    sock.sendto(bytes(64), address)
+    data = sampler.process()
+    assert sampler._packets_collected == 3
+    assert data[0] == 0     # should return <MessageType.RawData: 0>
+    assert data[1][1] == 1  # provided longtime gets returned
+    assert len(data[1][0]) == 16
+    assert data[1][0].sum() == 0
+
+    # send 10x 1
+    sampler.preRun()
+    time.sleep(0.4)
+    sock.sendto(np.array(10*[1], dtype=np.uint64).tobytes(), address)
+    data = sampler.process()
+    assert sampler._packets_collected == 4
+    assert data[0] == 0     # should return <MessageType.RawData: 0>
+    assert data[1][1] == 1  # provided longtime gets returned
+    assert len(data[1][0]) == 10
+    assert data[1][0].sum() == 10
+
+    # send 1000x 10
+    sampler.preRun()
+    time.sleep(0.4)
+    sock.sendto(np.array(1000 * [10], dtype=np.uint64).tobytes(), address)
+    data = sampler.process()
+    assert sampler._packets_collected == 5
+    assert data[0] == 0  # should return <MessageType.RawData: 0>
+    assert data[1][1] == 1  # provided longtime gets returned
+    assert len(data[1][0]) == 1000
+    assert data[1][0].sum() == 10000
+
+def test_queue():
+    '''
+    test functionality of 1st aquisition pipeline step with data been put into queue
+    '''
 
 
+
+if __name__ == "__main__":
+    test_receive()
