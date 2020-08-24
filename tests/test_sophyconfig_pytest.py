38cce59146c9465d4ee6d14b0833ed72e2add5f2
Finn Rodenberg <finn.rodenberg@cfel.de>
2020-08-24 16:45:42 +0200

    Continue work on SoPhy test

--- tests/test_sophyconfig_pytest.py
+++ tests/test_sophyconfig_pytest.py
@@ -1,7 +1,11 @@
 import socket
 import socketserver
+import threading
 import numpy as np
+from pymepix import Pymepix
 from pymepix.config.sophyconfig import SophyConfig
+from pymepix.util.spidrDummyTCP import TPX3Handler
+
 
 CONFIG_PATH = '/Users/rodenbef/desy/ASI_TPX3CAM_H06/W0028_H06/settings/W0028_H06_50V.spx'
 ADDRESS = ("192.168.1.10", 50000)
@@ -18,26 +22,33 @@ def test_parameters():
         elif code in [6, 16]:
             assert 0 <= value <= 511
         elif code == 18:
-            pass  # TODO: @firode find out boundaries for PLL_VCNTRL (default is 128)
+            pass  # TODO: @firode waiting for answer with information about PLL_VCNTRL
 
 
 def test_pixelmask():
     """Check whether the pixelmask is in an appropriate format"""
-    # korrekter Wertebereich?
-    # ergibt Sinn > mehr unmaskierte als maskierte?
-    pass
+    spx = SophyConfig(CONFIG_PATH)
+    mask = spx.maskPixels()
+    test = spx.testPixels()
+    thresh = spx.thresholdPixels()
+
+    # check for correct range of values
+    assert mask.min() >= 0 and mask.max() <= 1
+    assert test.min() >= 0 and test.max() <= 256
+    assert thresh.min() >= 0 and thresh.max() <= 15
+    assert np.count_nonzero(mask == 0) > np.count_nonzero(mask == 1)
+    print(np.count_nonzero(mask == 1))
 
 
-class TPX3PacketCapture(socketserver.BaseRequestHandler):
+class TPX3PacketCapture(TPX3Handler):
     """The part to capture and evaluate the config packets from pymepix"""
 
     def handle(self):
-        while True:
-            sock_recv = self.request.recv(1024)
-            self.data = np.frombuffer(sock_recv, dtype=np.uint32)
-
-            if len(self.data) > 0:
-                cmd = socket.htonl(int(self.data[0]))
+        self.requestIndex = 0
+        # while self.requestIndex<1000:
+        while 1:
+            self._gather_packet()
+            self._process_data()
 
                 # nocheinmal Sophy Pakete abfangen, als Referenz ansehen!
                 # Cmd Code an richtiger Stelle?
@@ -49,10 +60,22 @@ def test_send_config():
     """Pretend to be a TPX3 and capture config packets.
     Check for the right format.
     """
-    # socket zum annehmen erstellen
+    print("Starting server...")  # hier stimmt noch etwas nicht
+    server = socketserver.TCPServer(ADDRESS, TPX3PacketCapture)
+    thread = threading.Thread(target=server.serve_forever)
+    thread.start()
 
-    pass
+    print("Initializing Pymepix...")
+    tpx = Pymepix(ADDRESS)
+    tpx[0].loadConfig(CONFIG_PATH)
+
+    print("Starting Pymepix...")
+    tpx.start()
+    tpx.stop()
+
+    print("Shutting down server")
+    server.server_close()
 
 
 if __name__ == "__main__":
-    test_parameters()
+    test_send_config()
