d616cba81068df7036a688c32a2a1b832938c8af
BenMoon <brombh@desy.de>
2020-08-27 16:59:39 +0200

    move tcpsampler to utils as it is just for testing purpose

--- tests/test_tcpsampler_pytest.py
+++ tests/test_tcpsampler_pytest.py
@@ -29,7 +29,7 @@ from multiprocessing import Queue
 
 import numpy as np
 from pymepix.processing.acquisition import AcquisitionPipeline
-from pymepix.processing.tcpsampler import TcpSampler
+from pymepix.util.tcpsampler import TcpSampler
 
 address = ('127.0.0.1', 50000)
 
