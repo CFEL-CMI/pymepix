42d6463d24d816c2d274502b4dac060ab0a55cf4
Tobias Röhling <tobias.roehling@cfel.de>
2021-09-20 16:25:29 +0200

    #18 Progress bar for CLI post processing implemented

--- pymepix/post_processing.py
+++ pymepix/post_processing.py
@@ -1,5 +1,14 @@
+from tqdm import tqdm
+
 from pymepix.processing.rawfilesampler import RawFileSampler
 
-def run_post_processing(input_file_name, output_file, number_processes, timewalk_file, cent_timewalk_file, progress_callback=None):
-    file_sampler = RawFileSampler(input_file_name, output_file, number_processes, timewalk_file, cent_timewalk_file, progress_callback)
-    file_sampler.run()
+class ProgressBar(tqdm):
+
+    def update_to(self, progress):
+        return self.update(progress - self.n)
+
+
+def run_post_processing(input_file_name, output_file, number_processes, timewalk_file, cent_timewalk_file):
+    with ProgressBar(total=1.0, dynamic_ncols=True) as progress_bar:
+        file_sampler = RawFileSampler(input_file_name, output_file, number_processes, timewalk_file, cent_timewalk_file, progress_bar.update_to)
+        file_sampler.run()
