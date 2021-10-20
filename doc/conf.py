9b0af097534d7f24a42b4f0b26c703ed2cc8abeb
Tobias Röhling <tobias.roehling@cfel.de>
2021-10-20 10:55:59 +0200

    Code missing in documentation path for autodoc

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ with open(setup_path, "r") as f:
             desired.remove(line_list[0])
 project = name
 
-sys.path.insert(0, os.path.abspath("../pymepix/"))
+sys.path.insert(0, os.path.abspath("../"))
 print(sys.path)
 
 # -- General configuration ------------------------------------------------
