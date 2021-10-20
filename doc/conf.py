dfe8ee88067230be3a3428e77968f361766af519
Tobias Röhling <tobias.roehling@cfel.de>
2021-10-20 12:37:59 +0200

    Fix dependencies for documentation

--- doc/conf.py
+++ doc/conf.py
@@ -42,6 +42,7 @@ with open(setup_path, "r") as f:
             desired.remove(line_list[0])
 project = name
 
+sys.path.insert(0, os.path.abspath("../../lib"))
 sys.path.insert(0, os.path.abspath("../"))
 print(sys.path)
 
