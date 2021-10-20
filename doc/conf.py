161c45075e4780a507d705968063db94be7498df
Tobias Röhling <tobias.roehling@cfel.de>
2021-10-20 14:34:50 +0200

    Maybe fix readthedocs documentation

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,8 @@ with open(setup_path, "r") as f:
             desired.remove(line_list[0])
 project = name
 
-sys.path.insert(0, os.path.abspath("../pymepix"))
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
 
 # -- General configuration ------------------------------------------------
 
