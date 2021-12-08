3a86810e7107a57005e5ea6f2daf3596f5eda252
BenMoon <stonemonkey@gmx.de>
2021-12-08 14:12:03 +0100

    Fix post processing and progessbar update

--- pymepix/post_processing.py
+++ pymepix/post_processing.py
@@ -3,12 +3,15 @@ from tqdm import tqdm
 from pymepix.processing.rawfilesampler import RawFileSampler
 
 class ProgressBar(tqdm):
+    gui_bar_fun = None
 
     def update_to(self, progress):
+        self.gui_bar_fun(self.n)
         return self.update(progress - self.n)
 
 
-def run_post_processing(input_file_name, output_file, number_processes, timewalk_file, cent_timewalk_file):
+def run_post_processing(input_file_name, output_file, number_processes, timewalk_file, cent_timewalk_file, progress_callback):
     with ProgressBar(total=1.0, dynamic_ncols=True) as progress_bar:
+        progress_bar.gui_bar_fun = progress_callback
         file_sampler = RawFileSampler(input_file_name, output_file, number_processes, timewalk_file, cent_timewalk_file, progress_bar.update_to)
         file_sampler.run()
