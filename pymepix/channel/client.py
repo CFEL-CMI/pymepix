a67ed9e40359179e9f0606611fe69e1d6b1ceaf9
samartse <andrey.samartsev@desy.de>
2023-02-14 16:30:21 +0100

    client update

--- pymepix/channel/client.py
+++ pymepix/channel/client.py
@@ -12,16 +12,35 @@ import queue
 #while True:
 #    data = data_queue.get()
 #    .....
+# one can also suppy the callback function
+
+
+import threading
+import zmq
+import numpy as np
+
+import queue
+
+from enum import Enum
+
+
+# class syntax
+class ChannelDataType(Enum):
+    COMMAND = 1
+    PIXEL = 2
+    TOF = 3
+    CENTROID = 4
 
 
 class Client(threading.Thread):
 
-    def __init__(self, channel_address):
+    def __init__(self, channel_address, callback=None, queue_maxsize=32):
         threading.Thread.__init__(self)
         self.daemon = True
-        self.socket = None
-        self.address = None
-        self.q = queue.Queue(maxsize=16)
+        self._socket = None
+        self._address = channel_address
+        self._callback = callback
+        self.q = queue.Queue(queue_maxsize)
         self.register(channel_address)
         self.running = True
         self.start()
@@ -40,14 +59,18 @@ class Client(threading.Thread):
         host, s_port = channel_address.split("//")[-1].split(":")
         self.port = int(s_port)
         context = zmq.Context()
-        self.socket = context.socket(zmq.SUB)
+        self._socket = context.socket(zmq.SUB)
 
+        print('connecting to address: ', "tcp://{}:{}".format(host, port))
         # Connects to a bound socket
-        self.socket.connect("tcp://{}:{}".format(host, port))
-        self.socket.subscribe("")
+        self._socket.connect("tcp://{}:{}".format(host, port))
+        self._socket.subscribe("")
 
     def run(self):
+        print('start receiving data')
         while self.running:
-            obj = self.socket.recv_pyobj()
+            obj = self._socket.recv_pyobj()
             if not self.q.full():
                 self.q.put_nowait(obj)
+            if self._callback != None:
+                self._callback(obj)
