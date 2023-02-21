9e72ee81d08a0e6386d00eb49e319a2e89d60895
samartse <andrey.samartsev@desy.de>
2023-02-21 16:04:04 +0100

    tcp client update

--- pymepix/channel/client.py
+++ pymepix/channel/client.py
@@ -1,36 +1,7 @@
 import threading
 import zmq
-import queue
-
-#Use:
-
-#host = "127.0.0.1"
-#port = "5556"
-#client = Client("tcp://{}:{}".format(host, port))
-
-#data_queue = client.get_queue()
-#while True:
-#    data = data_queue.get()
-#    .....
-# one can also suppy the callback function
-
-
-import threading
-import zmq
-import numpy as np
 
 import queue
-
-from enum import Enum
-
-
-# class syntax
-class ChannelDataType(Enum):
-    COMMAND = 1
-    PIXEL = 2
-    TOF = 3
-    CENTROID = 4
-
 
 class Client(threading.Thread):
 
@@ -38,7 +9,7 @@ class Client(threading.Thread):
         threading.Thread.__init__(self)
         self.daemon = True
         self._socket = None
-        self._address = channel_address
+        self._address = "tcp://{}:{}".format(*channel_address)
         self._callback = callback
         self.q = queue.Queue(queue_maxsize)
         self.register(channel_address)
@@ -52,18 +23,16 @@ class Client(threading.Thread):
         return self.q
 
     def public_address(self):
-        return f"tcp://{self.socket.gethostname()}:{self.port}"
+        return f"tcp://{self.address[0]}:{self.address[0]}"
 
     def register(self, channel_address):
         self.address = channel_address
-        host, s_port = channel_address.split("//")[-1].split(":")
-        self.port = int(s_port)
         context = zmq.Context()
         self._socket = context.socket(zmq.SUB)
 
-        print('connecting to address: ', "tcp://{}:{}".format(host, port))
+        print('connecting to address: ', "tcp://{}:{}".format(*channel_address))
         # Connects to a bound socket
-        self._socket.connect("tcp://{}:{}".format(host, port))
+        self._socket.connect("tcp://{}:{}".format(*channel_address))
         self._socket.subscribe("")
 
     def run(self):
